8c11a0b74808da5d4ab12ab4cc9151e8a86d0e33
MichalKrol2020 <106864921+MichalKrol2020@users.noreply.github.com>
2023-03-26 14:00:51 +0200

    Update InvalidTokenException.java

--- src/main/java/com/company/exception/InvalidTokenException.java
+++ src/main/java/com/company/exception/InvalidTokenException.java
@@ -1,6 +1,6 @@
 package com.company.exception;
 
-public class InvalidTokenException extends RuntimeException
+public class InvalidTokenException extends Exception
 {
     public InvalidTokenException(String message)
     {
